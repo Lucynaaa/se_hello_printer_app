f7f54f5a5e6baf81b192bcb0770b6b51ed0871cd
Lucynaaa <33197568+Lucynaaa@users.noreply.github.com>
2020-10-05 21:51:59 +0200

    test multiple outputes removed

--- test/test_views.py
+++ test/test_views.py
@@ -1,17 +1,11 @@
 import unittest
 from hello_world import app
-from hello_world.formater import SUPPORTED
-
 
 class FlaskrTestCase(unittest.TestCase):
     def setUp(self):
         app.config['TESTING'] = True
         self.app = app.test_client()
 
-    def test_outputs(self):
-        rv = self.app.get('/outputs')
-        ','.join(SUPPORTED) in rv.data
-
     def test_msg_with_output(self):
-        rv = self.app.get('/?output=json')
-        self.assertEquals('{ "imie":"Lucy&Mike", "mgs":Hello World!"}', rv.data)
+        response = self.app.get('/')
+        self.assertEquals("Hello: Lucy&Mike", response.data)
