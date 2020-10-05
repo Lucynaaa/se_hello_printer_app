6fe95c6c5136a45b205b93b4b13cbf11343758cd
Lucynaaa <33197568+Lucynaaa@users.noreply.github.com>
2020-10-05 21:27:42 +0200

    output formater  removed
    output formater  removed

--- hello_world/views.py
+++ hello_world/views.py
@@ -1,20 +1,23 @@
 from hello_world import app
-from formater import get_formatted
-from formater import SUPPORTED, PLAIN
+#from formater import get_formatted
+#from formater import SUPPORTED, PLAIN
 from flask import request
 
 moje_imie = "Lucy&Mike"
-msg = "Hello World!"
+msg = "Hello: "
 
 
 @app.route('/')
 def index():
-    output = request.args.get('output')
-    if not output:
-        output = PLAIN
-    return get_formatted(msg, moje_imie,
-                         output.lower())
+   return msg + ' ' + moje_imie 
 
+#@app.route('/')
+#def index():
+#    output = request.args.get('output')
+#    if not output:
+#        output = PLAIN
+#    return get_formatted(msg, moje_imie,
+#                         output.lower())
 
 @app.route('/outputs')
 def supported_output():
